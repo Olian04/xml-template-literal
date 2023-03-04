cdbc847a7aab63b330281b96ed75afe0135e67ec
Olian04 <rift95@live.se>
2023-03-04 14:17:04 +0100

    ..

--- src/api.ts
+++ src/api.ts
@@ -1,39 +1,30 @@
-import type { AstChild } from '!types/AbstractSyntaxTree';
-import type { SegmentStream } from '!types/SegmentStream';
-
 import { parseTokens } from '!parser/index';
 import { tokenizer } from '!tokenizer/index';
 import { mergeTemplateSegments } from '!util/mergeTemplateSegments';
 
 export type {
+  AstKind,
   AstChild,
   AstAttribute,
+  AstAttributeComposite,
   ChildType,
   AttributeType,
 } from '!types/AbstractSyntaxTree';
 
-export function parseXml(xmlString: string): AstChild<never>;
-export function parseXml<T>(segments: SegmentStream<T>): AstChild<T>;
-export function parseXml(
-  stringOrSegment: string | SegmentStream<unknown>
-): AstChild<unknown> {
-  if (typeof stringOrSegment === 'string') {
-    return parseTokens(
-      tokenizer(
-        mergeTemplateSegments({
-          dynamic: [],
-          static: [stringOrSegment],
-        })
-      )
-    );
-  }
-  return parseTokens(tokenizer(stringOrSegment));
-}
+export const parseXml = (xmlString: string) =>
+  parseTokens(
+    tokenizer(
+      mergeTemplateSegments({
+        dynamic: [],
+        static: [xmlString],
+      })
+    )
+  );
 
 export const xml = <T>(
   staticSegments: TemplateStringsArray,
   ...dynamicSegments: T[]
-): AstChild<T> =>
+) =>
   parseTokens(
     tokenizer(
       mergeTemplateSegments({
