eeb4991b4b6689d291cfeab8fb3bf2cc10b3cb89
Olian04 <rift95@live.se>
2023-03-04 12:36:48 +0100

    ..

--- .mocharc.js
+++ .mocharc.js
@@ -2,7 +2,8 @@ const { MochaOptions } = require('mocha');
 
 /** @type {MochaOptions} */
 module.exports = {
+  parallel: true,
   spec: ['./tests/**/*.test.ts', './tests/**/*.spec.ts'],
-  require: ['ts-node/register'],
+  require: ['ts-node/register', 'tsconfig-paths/register'],
   recursive: true,
 };
