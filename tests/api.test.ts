a40219ad2abc86d25cf97dfd4fed45ecfae5acfb
Oliver Linnarsson <oliver@lolo.company>
2023-05-19 15:36:29 +0200

    feat: Add failing api test for new bug

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 
-import { xml, AstKind, ChildType } from '../dist/api';
+import { xml, AstKind, ChildType, AttributeType } from '../dist/api';
 
 describe('api', () => {
   it('should expose XML template literal function', () => {
@@ -76,6 +76,101 @@ describe('api', () => {
               type: ChildType.Text,
               value: '!',
             },
+          ],
+        },
+      ],
+    });
+  });
+
+  it('should work for a minimal-complete-example', () => {
+    const func = _ => _;
+    const ast = xml`
+      <div class="widget">
+        <h1>Some Title</h1>
+        <form>
+          <input type="text" name="something" />
+          <input type="submit" onclick=${func} />
+        </form>
+      </div>
+    `;
+
+    expect(ast).to.deep.equal({
+      kind: AstKind.Root,
+      children: [
+        {
+          kind: AstKind.Child,
+          type: ChildType.Node,
+          tag: 'div',
+          attributes: [
+            {
+              kind: AstKind.Attribute,
+              type: AttributeType.Text,
+              key: 'class',
+              value: 'widget',
+            }
+          ],
+          children: [
+            {
+              kind: AstKind.Child,
+              type: ChildType.Node,
+              tag: 'h1',
+              attributes: [],
+              children: [
+                {
+                  kind: AstKind.Child,
+                  type: ChildType.Text,
+                  value: 'Some Title',
+                }
+              ],
+            },
+            {
+              kind: AstKind.Child,
+              type: ChildType.Node,
+              tag: 'form',
+              attributes: [],
+              children: [
+                {
+                  kind: AstKind.Child,
+                  type: ChildType.Node,
+                  tag: 'input',
+                  attributes: [
+                    {
+                      kind: AstKind.Attribute,
+                      type: AttributeType.Text,
+                      key: 'type',
+                      value: 'text',
+                    },
+                    {
+                      kind: AstKind.Attribute,
+                      type: AttributeType.Text,
+                      key: 'name',
+                      value: 'something',
+                    }
+                  ],
+                  children: [],
+                },
+                {
+                  kind: AstKind.Child,
+                  type: ChildType.Node,
+                  tag: 'h1',
+                  attributes: [
+                    {
+                      kind: AstKind.Attribute,
+                      type: AttributeType.Text,
+                      key: 'type',
+                      value: 'submit',
+                    },
+                    {
+                      kind: AstKind.Attribute,
+                      type: AttributeType.Data,
+                      key: 'onclick',
+                      value: func,
+                    }
+                  ],
+                  children: [],
+                }
+              ],
+            }
           ],
         },
       ],
